926806b5be58019c46450470944b224171107406
Chengm <18179739967@163.com>
2019-12-07 12:28:19 +0800

    commit

---
+++ app/src/main/java/com/chengm/pirate/MainActivity.java
@@ -0,0 +1,18 @@
+package com.chengm.pirate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * author : ChenWJ
+ * date : 2019/12/7 10:43
+ * description :
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
